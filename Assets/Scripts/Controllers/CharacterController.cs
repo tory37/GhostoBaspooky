2dfd34c20c793ff98054b8cffb7df613a752e545
tory37 <tory37@gmail.com>
2015-09-03 12:35:14 -0500

    Character Controller Inspector
    Worked on the custom inspector for the character controller

--- Assets/Scripts/Controllers/CharacterController.cs
+++ Assets/Scripts/Controllers/CharacterController.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
 
 public class CharacterController : MonoBehaviour {
 
 	#region Enums
 	
-	public enum MovementType
+	public enum MovementTypes
 	{
-		Walk,
-		Hop
+		/// <summary>
+		/// Stays on the ground and walks/runs left and right
+		/// </summary>
+		Walker,
+		/// <summary>
+		/// Someone who has free vertical and horizontal range,
+		/// though this can be limited by a 0 speed in either direction
+		/// </summary>
+		Flier,
+		/// <summary>
+		/// Someone who is limited to a distance from the ground, 
+		/// but never touches it/ they float
+		/// </summary>
+		Hoverer,
+		/// <summary>
+		/// Someone who uses a jetpack, so has a hybrid of hovering and flying,
+		/// and moves upward when using a boost, etc
+		/// </summary>
+		Jetpack
 	}
 
 	#endregion
@@ -17,7 +35,7 @@ public class CharacterController : MonoBehaviour {
 	#region Editor Interface
 
 	[Serializable]
-	protected class Components
+	public class NeededComponents
 	{
 		public Rigidbody rigidBody;
 		public Animator animator;
@@ -25,41 +43,97 @@ public class CharacterController : MonoBehaviour {
 	}
 
 	[Serializable]
-	protected class MovementProperties
+	public class MovementProperties
 	{
 		public float horizontalSpeed;
 		public float verticalSpeed;
 	}
 
 	[Serializable]
-	protected class JumpProperties
+	public class JumpProperties
 	{
 		public float jumpHeight;
 		public bool canJump;
-		public bool canDoubleJump;
+		public int maxConsecutiveJumps;
 	}
 
 	[Serializable]
-	protected class AnimationProperties
+	public class AnimationProperties
 	{
 		public string horizontalMovement;
 		public string verticalMovement;
 		public string gotHurt;
 		public string firstJump;
 		public string secondJump;
+		public List<AnimParamToBool> animationChecks = new List<AnimParamToBool>
+		{
+			new AnimParamToBool { paramDescription = "Horizontal Movement", paramName = "", hasParam = false },
+			new AnimParamToBool { paramDescription = "Vertical Movement",   paramName = "", hasParam = false },
+			new AnimParamToBool { paramDescription = "Got Hurt",            paramName = "", hasParam = false },
+			new AnimParamToBool { paramDescription = "First Jump",          paramName = "", hasParam = false },
+			new AnimParamToBool { paramDescription = "Second Jump",         paramName = "", hasParam = false }
+		};
 	}
 
-	[SerializeField] private Components components;
+	[SerializeField] private MovementTypes movementType;
+	[SerializeField] private NeededComponents components;
 	[SerializeField] private MovementProperties pMovement;
 	[SerializeField] private JumpProperties pJump;
 	[SerializeField] private AnimationProperties pAnimation;
 
 	#endregion
 
+	#region Public Interface
+
+	public MovementTypes MovementType
+	{
+		get { return movementType; }
+		#if UNITY_EDITOR
+		set { movementType = value; }
+		#endif
+	}
+
+	public NeededComponents Components
+	{
+		get { return components; }
+		#if UNITY_EDITOR
+		set { components = value; }
+		#endif
+	}
+	public bool isComponentsExpanded;
+
+	public MovementProperties PMovement
+	{
+		get { return pMovement; }
+		#if UNITY_EDITOR
+		set { pMovement = value; }
+		#endif
+	}
+	public bool isPMovementExpanded;
+
+	public JumpProperties PJump
+	{
+		get { return pJump; }
+		#if UNITY_EDITOR
+		set { pJump = value; }
+		#endif
+	}
+	public bool isPJumpExpanded;
+
+	public AnimationProperties PAnimation
+	{
+		get { return pAnimation; }
+		#if UNITY_EDITOR
+		set { pAnimation = value; }
+		#endif
+	}
+	public bool isPAnimationExpanded;
+
+	#endregion
+
 	#region States
 
 	private State currentState = null;
-
 
 	private State StateDefault;
 
@@ -126,6 +200,8 @@ public class CharacterController : MonoBehaviour {
 
 	#endregion
 
+	#region Private Methods
+
 	private void Movement(Vector3 newPosition, Vector2 input)
 	{
 		components.rigidBody.MovePosition(newPosition);
@@ -146,4 +222,13 @@ public class CharacterController : MonoBehaviour {
 			return false;
 	}
 
+	#endregion
+
+	[Serializable]
+	public class AnimParamToBool
+	{
+		public string paramDescription;
+		public string paramName;
+		public bool hasParam;
+	}
 }
---
+++ Assets/Scripts/Controllers/CharacterControllerInspector.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CharacterController))]
+public class CharacterControllerInspector : Editor {
+
+
+	public override void OnInspectorGUI()
+	{
+		CharacterController controller = (CharacterController)target;
+
+		controller.isComponentsExpanded = EditorGUILayout.Foldout(controller.isComponentsExpanded, "Required Components");
+
+		//Seperator
+		GUILayout.Box("", new GUILayoutOption[]{GUILayout.ExpandWidth(true), GUILayout.Height(1)});
+
+		if (controller.isComponentsExpanded)
+		{
+			controller.Components.animator  = (Animator)EditorGUILayout.ObjectField("Animator", controller.Components.animator, typeof(Animator), true);
+			controller.Components.rigidBody = (Rigidbody)EditorGUILayout.ObjectField("Rigidbody", controller.Components.rigidBody, typeof(Rigidbody), true);
+			controller.Components.rotationVectors  = (RotationVectors)EditorGUILayout.ObjectField("Rotation Vectors", controller.Components.rotationVectors, typeof(RotationVectors), true);
+		}
+
+		controller.MovementType = (CharacterController.MovementTypes) EditorGUILayout.EnumPopup("Type of Movement", controller.MovementType);
+
+		controller.isPMovementExpanded = EditorGUILayout.Foldout(controller.isPMovementExpanded, "Movement Properties");
+		if (controller.isPMovementExpanded)
+		{
+			switch (controller.MovementType)
+			{
+				case CharacterController.MovementTypes.Walker:
+					controller.PMovement.horizontalSpeed = EditorGUILayout.FloatField("Horizontal Speed", controller.PMovement.horizontalSpeed);
+					break;
+
+				case CharacterController.MovementTypes.Flier:
+					controller.PMovement.horizontalSpeed = EditorGUILayout.FloatField("Horizontal Speed", controller.PMovement.horizontalSpeed);
+					controller.PMovement.verticalSpeed = EditorGUILayout.FloatField("Vertical Speed", controller.PMovement.verticalSpeed);
+					break;
+			}
+		}
+
+		if ( !controller.PJump.canJump )
+			controller.PJump.canJump = EditorGUILayout.Toggle("Can Jump", controller.PJump.canJump);
+		else
+		{
+			controller.isPJumpExpanded = EditorGUILayout.Foldout(controller.isPJumpExpanded, "Jump Properties");
+			if ( controller.isPJumpExpanded )
+			{
+				controller.PJump.canJump = EditorGUILayout.Toggle("Can Jump", controller.PJump.canJump);
+				controller.PJump.maxConsecutiveJumps = EditorGUILayout.IntField("Max Consecutive Jumps", controller.PJump.maxConsecutiveJumps);
+				controller.PJump.jumpHeight = EditorGUILayout.FloatField("Jump Height", controller.PJump.jumpHeight);
+			}
+		}
+		
+	}
+
+
+}
